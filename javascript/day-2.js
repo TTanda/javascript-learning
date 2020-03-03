4535794809f83be8a052a1f567c82485e724d617
Trina Tanda <medivalmusings@gmail.com>
2020-03-03 10:29:24 -0700

    day 2 addtions

---
+++ javascript/day-2.js
@@ -0,0 +1,45 @@
+/* day2.js*/
+
+var myNumber = "54.6"; //string
+console.log (myNumber + 6); //Concatenation occurs
+
+myNumber = parseInt ( myNumber ); //Now it is an integer
+
+console.log ( myNumber + 6 ); // Addition occurs
+
+myNumber = parseFloat( myNumber ); // Now it's a float 
+
+console.log ( myNumber + 6 ); // Addition occurs
+
+myNumber =Number( myNumber ); //Now it is a 64bit floating point numeric. The only actual number in JavaScript
+
+console.log( myNumber + 6); // Addition occurs
+
+/**
+ * Converting numbers to strings
+ */
+myNumber = myNumber.toString(); //The toString() method will turn a number into a string
+
+console.log( myNumber + 6 ); //Concatenation again
+/**
+ * Watch out, Auto-conversion can be interesting. Always check your bases
+ */
+var myString = "Hello World!";
+console.log ("Test1:");
+console.log ( myString);
+
+myString = 5;
+console.log ("Test 2:");
+console.log ( myString);
+
+myString = myString*3;
+console.log ("Test 3:");
+console.log ( myString);
+
+myString = "Updated string.";
+console.log ( "Test 4:");
+console.log (myString);
+
+myString = {"another" : "string test!?"};
+console.log ("Test 5:");
+console.log ( myString);
--- javascript/day2.js
+++
@@ -1,7 +0,0 @@
-r hotel = {
-    name: "The Quay",
-    totalRooms: 40,
-    bookedRooms: 25,
-    types: [
-        "twin",
-        "double",
