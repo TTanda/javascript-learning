c216c8a7e27e48df4dc9942e66b112d457bc2576
Trina Tanda <medivalmusings@gmail.com>
2020-03-11 11:04:13 -0600

    examples of .filter and .reduce method

--- day-7.js
+++ day-7.js
@@ -25,8 +25,8 @@ console.log( findAnIndex ); // This time, it returned the value's INDEX.
  * .entries()
  */
 var pairing = myArray.entries();
-console.log( pairing );
-console.log( pairing.next().value );
+console.log( pairing ); // Returns an iterator.
+console.log( pairing.next().value );// We can keep bumping to the next item, getting a nice key/value array to let ys know what's inside.
 console.log( pairing.next().value );
 console.log( pairing.next().value );
 console.log( pairing.next().value );
@@ -34,3 +34,29 @@ console.log( pairing.next().value );
 console.log( pairing.next().value );
 console.log( pairing.next().value );
 
+/**
+ * .from() method
+ */
+
+ var fromArray = Array.from( "ABCDEFGHIJKLMNOPQRSTUVWXYZ" );
+ console.log( fromArray ); // Oh geeze, that's a 26-element array now.
+ var fromArray2 = Array.from( "Hello, World!");
+ console.log( fromArray2 ); // Even spaces and special characters will captured as elements!
+
+ /**
+  * .filter() method:
+  */
+
+  var filtered = myArray.filter( function ( element ) { return element > 4; } );
+  console.log( filtered ); //All matches returned ( 5 and 38 )
+
+  /**
+   * .reduce() method:
+   * We can add on to a value, returns only a single.
+   */
+  var reduceResult = myArray.reduce( function ( accumulator, currentVal ) {
+      return accumulator + currentVal;
+  } );
+  console.log( reduceResult );// Because we have strings in here, they are getting concatenated
+
+  console.log([1, 2, 3, 4 ].reduce( function ( a, v ) { return a + v; } ) ); // Because we have numbers, they are being added mathmatically.
